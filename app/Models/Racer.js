cc0eee2dad9f0e78418cc526f143f2751e563859
Patrick Misner <misner.patrick@gmail.com>
2022-05-23 16:18:10 -0600

    lostagain

--- app/Models/Racer.js
+++ app/Models/Racer.js
@@ -1,2 +1,9 @@
 // racer class , racer position
+import { generateId } from "../Utils/generateId.js"
 
+export class Racer {
+  constructor(data) {
+    this.id = generateId()
+    this.title = data.title
+  }
+}
