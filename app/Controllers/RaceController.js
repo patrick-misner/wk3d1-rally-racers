82f8450ea0788b7c57319879c25fe4dd7c97202c
Patrick Misner <misner.patrick@gmail.com>
2022-05-23 16:31:45 -0600

    update

--- app/Controllers/RaceController.js
+++ app/Controllers/RaceController.js
@@ -1,8 +1,13 @@
 // race controller class 
 
+
+function draw(){
+  document.getElementById('1').innerText = 'zoop'
+}
 export class RaceController{
   constructor(){
     console.log('RaceController loaded')
+    draw()
   } 
 
 }
